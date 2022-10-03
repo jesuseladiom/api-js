4c33a3b9e79c95614d8f35b9055aace254dec5f8
jesuseladiom <jesuseladiom@gmail.com>
2022-10-03 16:18:39 -0600

    feat:actualizando controlador y servicio

--- src/controller/persons.controller.js
+++ src/controller/persons.controller.js
@@ -1,66 +1,55 @@
-//import { methods as productServices } from "../services/product.service";
-// *****************temporal va en el services
-import {getConnection} from './../common/connections';
-
-
-
-// -------------------------------------------------
+import { methods as personServices } from "../services/person.service";
 
 const getPersons= async (req,res) => {
     try {
-        const connection= await getConnection();
-        const result= await connection.query("select * from persons");
-        if (result.length) res.json(result);
-        else res.json({"message":"No data"});
-    } catch (error) {
-        res.status(500);
-        res.json({"MENSAJE":error.message});
-    }
+            const query = await personServices.readPersons(res);
+            if (query.length) res.json(query);
+            else res.json({"message":"No data"});
+        } catch (error) {
+                 res.status(500);
+                 res.send({"message":error.message});
+        }
 };
 
-const getPerson=  async (req,res) => {
+const getPerson = async (req, res)=>{
     try {
-        const {id}= req.params;
-        const connection= await getConnection();
-        const result= await connection.query("select * from persons where person_id = ?", id);
-        if (result.length) res.json(result);
-        else res.json({"message":"Person not found"});
+        const query = await personServices.readPerson(req.params,res);
+        if (query.length) res.json(query);
+        else res.json({"message":"Person not found"});    
     } catch (error) {
         res.status(400);
-        res.json({"message":error.message});
+        res.send({"message":error.message});
     }
 };
 
-
-const addPerson= async (req,res) => {
+const addPerson = async (req, res)=>{
+    let flagError=0;
     try {
-        //console.log(req.body);
         const {first_name, last_name, edad, genero}= req.body;
+        console.log("controlador")
         if (first_name===undefined || last_name===undefined|| edad===undefined|| genero===undefined) {
+            flagError= 1;
             res.status(400).json({"message":"Bad Request. Please fill all fields"});
         }
-        const person= {first_name, last_name, edad, genero};
-        const connection= await getConnection();
-        const result= await connection.query("insert into persons SET ?", person);
-        //res.json(result);
-        res.json({message:"Person added"});
+        const query = await personServices.addPerson(req.body,res);
+        if (query.affectedRows) res.status(201).json({"message":"Person added"});
+        else res.json({"message":"Person not added"});    
     } catch (error) {
-        res.status(500);
-        res.send({"message":error.message,"text":"Person not added"});
-        //res.json({"MENSAJE":error.message});
+        if (flagError) return;
+        else res.status(400).json({"message":error.message});
     }
 };
 
-const deletePerson= async (req,res) => {
+
+
+const deletePerson = async (req, res)=>{
     try {
-        const {id}= req.params;
-        const connection= await getConnection();
-        const result= await connection.query("delete from persons where person_id = ?", id);
-        if (result.affectedRows) res.json({"message":"Person deleted"});
-        else res.json({"message":"Person not found"});
+        const query = await personServices.deletePerson(req.params,res);
+        if (query.affectedRows) res.json({"message":"Person deleted"});
+        else res.json({"message":"Person not found"});    
     } catch (error) {
         res.status(400);
-        res.json({"message":error.message});
+        res.send({"message":error.message});
     }
 };
 
--- src/services/person.service.js
+++ src/services/person.service.js
@@ -1,11 +1,38 @@
-import {getConnection} from '../common/connections';
+import {getConnection} from "./../common/connections"
 
 async function readPersons(response) {
-    const connection= await getConnection();
-    const result= await connection.query("select * from persons")
-    return response.json(result);
+        const connection = await getConnection();
+        const result = await connection.query("SELECT * FROM persons")
+        return result;
 }
 
+async function readPerson(body,response) {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT * FROM persons WHERE person_id = ?", body.id)
+    return result;
+}
+
+async function deletePerson(body,response) {
+    const connection = await getConnection();
+    const result = await connection.query("DELETE FROM persons WHERE person_id = ?", body.id)
+    return result;
+}
+
+async function addPerson(body, response) {
+        const {first_name, last_name, edad, genero}= body;
+        const person= {first_name, last_name, edad, genero};
+        const connection = await getConnection();
+        const result= await connection.query("insert into persons SET ?", person);
+        return result;
+}
+
+
+
+
 export const methods = {
-    readPersons
+    addPerson,
+    readPersons,
+    readPerson,
+    //updateProduct,
+    deletePerson
 }
