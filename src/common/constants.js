213dacf338e609aa20ab680635acb487cfab1463
jesuseladiom <jesuseladiom@gmail.com>
2022-10-04 09:55:34 -0600

    "feat: Configuracion de la base de datos"

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,8 +1,13 @@
 const config= {
-    HOST:"localhost",
-    DB:"api-js",
-    USER:"root",
-    PWD:""
+    // HOST:"localhost",
+    // DB:"api-js",
+    // USER:"root",
+    // PWD:""
+    HOST: '31.220.104.219',
+    DB: 'u917498081_dev_f',
+    USER: 'u917498081_js_a',
+    PWD: '3st43sl4c0ntr4s3N14'
+
 } 
 
 export default config;
