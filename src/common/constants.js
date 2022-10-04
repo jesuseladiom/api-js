38c6cb539b05f40b4958d5e527e60da5dc7d2f28
jesuseladiom <jesuseladiom@gmail.com>
2022-10-04 15:24:14 -0600

    "feat: acceso a la bd"

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,9 +4,10 @@ const config= {
     // USER:"root",
     // PWD:""
     HOST: '31.220.104.219',
-    DB: 'u917498081_dev_f',
-    USER: 'u917498081_js_a',
-    PWD: '3st43sl4c0ntr4s3N14'
+    DB: 'u917498081_my_db',
+    USER: 'u917498081_user',
+    PWD: 'x1;7RPvCiQ'
+
 
 } 
 
