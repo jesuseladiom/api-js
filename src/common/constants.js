73bd511db2cbbf4703c791a6830f5ca8440d2304
jesuseladiom <jesuseladiom@gmail.com>
2022-10-04 16:11:07 -0600

    feat: produccion

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,10 +3,10 @@ const config= {
     // DB:"api-js",
     // USER:"root",
     // PWD:""
-    HOST: '31.220.104.219',
-    DB: 'u917498081_my_db',
-    USER: 'u917498081_user',
-    PWD: 'x1;7RPvCiQ'
+    HOST: '50.87.239.125',
+    DB: 'campesi1_curso',
+    USER: 'campesi1_curso',
+    PWD: 'Lara7806'
 
 
 } 
