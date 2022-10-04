f76e58f390f057df35e36a32dead915b24623a91
jesuseladiom <jesuseladiom@gmail.com>
2022-10-04 13:13:22 -0600

    "feat: acceso a la base de datos"

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,12 +1,12 @@
 const config= {
-    HOST:"localhost",
-    DB:"api-js",
-    USER:"root",
-    PWD:""
-    // HOST: '31.220.104.219',
-    // DB: 'u917498081_dev_f',
-    // USER: 'u917498081_js_a',
-    // PWD: '3st43sl4c0ntr4s3N14'
+    // HOST:"localhost",
+    // DB:"api-js",
+    // USER:"root",
+    // PWD:""
+    HOST: '31.220.104.219',
+    DB: 'u917498081_dev_f',
+    USER: 'u917498081_js_a',
+    PWD: '3st43sl4c0ntr4s3N14'
 
 } 
 
