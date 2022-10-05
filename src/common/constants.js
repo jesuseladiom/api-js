307ae03327e4542d65f669081e0f5152ce81798f
jesuseladiom <jesuseladiom@gmail.com>
2022-10-05 08:34:28 -0600

    "feat: acceso a bd"

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,16 +3,14 @@ const config= {
     // DB:"api-js",
     // USER:"root",
     // PWD:""
-    // HOST: '50.87.239.125',
-    // DB: 'campesi1_curso',
-    // USER: 'campesi1_curso',
-    // PWD: 'Lara7806'
-    HOST: '31.220.104.219',
-    DB: 'u917498081_my_db',
-    USER: 'u917498081_user',
-    PWD: 'x1;7RPvCiQ'
-
-
+    HOST: '50.87.239.125',
+    DB: 'campesi1_curso',
+    USER: 'campesi1_curso',
+    PWD: 'Lara7806'
+    // HOST: '31.220.104.219',
+    // DB: 'u917498081_my_db',
+    // USER: 'u917498081_user',
+    // PWD: 'x1;7RPvCiQ'
 } 
 
 export default config;
