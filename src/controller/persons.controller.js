0fd85f32d77fee107aeeefba892bfaa5149990d5
jesuseladiom <jesuseladiom@gmail.com>
2022-10-05 15:21:46 -0600

    "feat: actualizar status de respuestas"

--- src/controller/persons.controller.js
+++ src/controller/persons.controller.js
@@ -3,8 +3,8 @@ import { methods as personServices } from "../services/person.service";
 const getPersons= async (req,res) => {
     try {
             const query = await personServices.readPersons(res);
-            if (query.length) res.json(query);
-            else res.json({"message":"No data"});
+            if (query.length) res.status(200).json(query);
+            else res.status(404).json({"message":"No data"});
         } catch (error) {
                  res.status(500);
                  res.send({"message":error.message});
@@ -14,10 +14,10 @@ const getPersons= async (req,res) => {
 const getPerson = async (req, res)=>{
     try {
         const query = await personServices.readPerson(req.params,res);
-        if (query.length) res.json(query);
-        else res.json({"message":"Person not found"});    
+        if (query.length) res.status(200).json(query);
+        else res.status(404).json({"message":"Person not found"});    
     } catch (error) {
-        res.status(400);
+        res.status(500);
         res.send({"message":error.message});
     }
 };
@@ -32,20 +32,20 @@ const addPerson = async (req, res)=>{
         }
         const query = await personServices.addPerson(req.body,res);
         if (query.affectedRows) res.status(201).json({"message":"Person added"});
-        else res.json({"message":"Person not added"});    
+        else res.status(400).json({"message":"Person not added"});    
     } catch (error) {
         if (flagError) return;
-        else res.status(400).json({"message":error.message});
+        else res.status(500).json({"message":error.message});
     }
 };
 
 const deletePerson = async (req, res)=>{
     try {
         const query = await personServices.deletePerson(req.params,res);
-        if (query.affectedRows) res.json({"message":"Person deleted"});
-        else res.json({"message":"Person not found"});    
+        if (query.affectedRows) res.status(200).json({"message":"Person deleted"});
+        else res.status(404).json({"message":"Person not found"});    
     } catch (error) {
-        res.status(400);
+        res.status(500);
         res.send({"message":error.message});
     }
 };
@@ -62,10 +62,10 @@ const updatePerson= async (req,res) => {
         }
         const query = await personServices.updatePerson(id,req.body,res);
         if (query.affectedRows) res.status(200).json({"message":"Person updated"});
-        else res.json({"message":"Person not updated"});    
+        else res.status(404).json({"message":"Person not found"});    
     } catch (error) {
         if (flagError) return;
-        else res.status(400).json({"message":error.message});
+        else res.status(500).json({"message":error.message});
     }
 };
 
@@ -81,11 +81,11 @@ const patchPerson= async (req,res) => {
             res.status(400).json({"message":"Bad Request. Fill at least one field"});
         }
         const query = await personServices.patchPerson(id,req.body,res);
-        if (query.affectedRows) res.status(200).json({"message":"Person patched"});
-        else res.json({"message":"Person not patched"});    
+        if (query.affectedRows) res.status(200).json({"message":"Person updated partial"});
+        else res.status(404).json({"message":"Person not found"});    
     } catch (error) {
         if (flagError) return;
-        else res.status(400).json({"message":error.message});
+        else res.status(500).json({"message":error.message});
     }
 };
 
