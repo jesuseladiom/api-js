fa49ba9f5db8ef9da8b463ab16ecdddcdab247d3
jesuseladiom <jesuseladiom@gmail.com>
2022-10-05 21:23:06 -0600

    "feat: Modificar Services"

--- src/services/person.service.js
+++ src/services/person.service.js
@@ -2,19 +2,19 @@ import {getConnection} from "./../common/connections"
 
 async function readPersons(response) {
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM persons")
+        const result = await connection.query("SELECT * FROM persons");
         return result;
 }
 
 async function readPerson(body,response) {
     const connection = await getConnection();
-    const result = await connection.query("SELECT * FROM persons WHERE person_id = ?", body.id)
+    const result = await connection.query("SELECT * FROM persons WHERE person_id = ?", body.id);
     return result;
 }
 
 async function deletePerson(body,response) {
     const connection = await getConnection();
-    const result = await connection.query("DELETE FROM persons WHERE person_id = ?", body.id)
+    const result = await connection.query("DELETE FROM persons WHERE person_id = ?", body.id);
     return result;
 }
 
